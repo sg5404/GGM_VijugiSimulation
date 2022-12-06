b16a918c0a27d290e8dfa72e852d03a532150738
kjp2981 <kjp29816265@gmail.com>
2022-12-06 23:34:46 +0900

    배틀 씬에서 정보 넘기기?

--- Assets/02.Scripts/Scenes/BattleScene.cs
+++ Assets/02.Scripts/Scenes/BattleScene.cs
@@ -1,10 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public class UIStract
+{
+    public string name;
+    public int level;
+    public int maxHp;
+    public int curHp;
+    public Slider hpBar;
+    public Slider levelBar;
+
+    public UIStract(string name, int level, int maxHp, int curHp, Slider hpBar, Slider levelBar)
+    {
+        this.name = name;
+        this.level = level;
+        this.maxHp = maxHp;
+        this.curHp = curHp;
+        this.hpBar = hpBar;
+        this.levelBar = levelBar;
+    }
+}
 
 public class BattleScene : BaseScene
 {
+    [SerializeField]
+    private GameObject _playerInfoPanel;
+    [SerializeField]
+    private GameObject _enemyInfoPanel;
+
+    private UIStract _playerUIStract;
+    private UIStract _enemyUIStract;
+
     private EnemyInfo enemyInfo;
+
+    private PokeInformationSO _playerPokemon;
+    private PokeInformationSO _enemyPokemon;
 
     protected override void Init()
     {
@@ -14,13 +46,35 @@ public class BattleScene : BaseScene
 
         enemyInfo = Managers.Save.LoadJsonFile<EnemyInfo>();
 
-        foreach (var info in enemyInfo.enemyPokemonList)
-        {
-            Debug.Log($"이름:{info.name}, 레벨:{info.Level}, 공격력:{info.CurrentAttack}, 방어력:{info.CurrentDefense}, 체력:{info.CurrentHP}");
-        }
+        //foreach (var info in enemyInfo.enemyPokemonList)
+        //{
+        //    Debug.Log($"이름:{info.name}, 레벨:{info.Level}, 공격력:{info.CurrentAttack}, 방어력:{info.CurrentDefense}, 체력:{info.CurrentHP}");
+        //}
+
+        _enemyPokemon = enemyInfo.enemyPokemonList[0];
+
+        _playerUIStract = new UIStract(_enemyPokemon.name, _enemyPokemon.Level, (int)_enemyPokemon.pokeHP, _enemyPokemon.CurrentHP, _enemyInfoPanel.transform.Find("HP Bar").GetComponent<Slider>(), _enemyInfoPanel.transform.Find("Exp Bar").GetComponent<Slider>());
 
         // TODO : 1. UI에 정보 연결
         // TODO : 2. UI Amination
+    }
+
+    public void SetUI(UIStract ui, bool isEnemy)
+    {
+        Slider hpBar;
+        Slider levelBar;
+        Text nameText;
+        Text levelText;
+        Text hpText;
+
+        if(isEnemy == true)
+        {
+            _enemyInfoPanel
+        }
+        else
+        {
+            _playerInfoPanel;
+        }
     }
 
     public override void Clear()
