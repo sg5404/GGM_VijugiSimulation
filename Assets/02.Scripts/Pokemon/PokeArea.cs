0989eab6a6d8ecb45f027f1f2bd7f54ff99ad102
sg5404 <ggm.s210028@ggh.goe.go.kr>
2022-12-19 09:19:35 +0900

    맵, 에셋

--- Assets/02.Scripts/Pokemon/PokeArea.cs
+++ Assets/02.Scripts/Pokemon/PokeArea.cs
@@ -6,7 +6,7 @@ using NaughtyAttributes;
 public class PokeArea : MonoBehaviour
 {
     [SerializeField] private int pokePercent;
-    [SerializeField] private List<PokeInformationSO> poke;
+    //[SerializeField] private List<PokeInformationSO> poke;
     [SerializeField] private List<PokemonInfoSO> pokemonList;
     [SerializeField, MinValue(1), MaxValue(100)] private int minLevel;
     [SerializeField, MinValue(1), MaxValue(100)] private int maxLevel;
@@ -26,8 +26,8 @@ public class PokeArea : MonoBehaviour
         if (control.timer < 1f) return;
         control.timer = 0;
         if (Random.Range(0, 100.0f) > pokePercent) return;
-        SetPokeMon();
-        Pokemon wildPokemon = new Pokemon(pokemonList[pokeNum], SetLevel());
+
+        Pokemon wildPokemon = new Pokemon(SetPokeMon(), SetLevel());
         GameInfo info = new GameInfo();
         info.PlayerInfo = player.GetInfo();
         info.isWildPokemon = true;
@@ -36,10 +36,11 @@ public class PokeArea : MonoBehaviour
         Managers.Scene.LoadScene(Define.Scene.Battle);
     }
 
-    void SetPokeMon() //³ªÁß¿¡ ·¹¾î¸®Æ¼¿¡ µû¶ó¼­ ³ª¿À´Â È®·ü ´Ù¸£°Ô ÇØÁà¾ßÇÔ
+    PokemonInfoSO SetPokeMon() //í¬ì¼“ëª¬ ì§€ì •
     {
         //pokeNum = Random.Range(0, poke.Count);
         pokeNum = Random.Range(0, pokemonList.Count);
+        return pokemonList[pokeNum];
     }
 
     //void SetLevel()
@@ -53,21 +54,21 @@ public class PokeArea : MonoBehaviour
         return Random.Range(minLevel, maxLevel + 1);
     }
 
-    void LevelStats()
-    {
-        var pokeMon = poke[pokeNum];
-        int basicStat = pokeMon.Rarity switch
-        {
-            Define.PokeRarity.Common => 10,
-            Define.PokeRarity.Rare => 15,
-            Define.PokeRarity.unique => 20,
-            Define.PokeRarity.Legendary => 30,
-            _ => 10,
-        };
+    //void LevelStats()
+    //{
+    //    var pokeMon = poke[pokeNum];
+    //    int basicStat = pokeMon.Rarity switch
+    //    {
+    //        Define.PokeRarity.Common => 10,
+    //        Define.PokeRarity.Rare => 15,
+    //        Define.PokeRarity.unique => 20,
+    //        Define.PokeRarity.Legendary => 30,
+    //        _ => 10,
+    //    };
 
-        pokeMon.CurrentAttack = basicStat + (int)(pokeMon.Level * pokeMon.PokeAttack);
-        pokeMon.CurrentDefense = basicStat + (int)(pokeMon.Level * pokeMon.PokeDefense);
-        pokeMon.CurrentHP = basicStat + (int)(pokeMon.Level * pokeMon.PokeHP);
-        pokeMon.CurrentSpeed = basicStat + (int)(pokeMon.Level * pokeMon.PokeSpeed);
-    }
+    //    pokeMon.CurrentAttack = basicStat + (int)(pokeMon.Level * pokeMon.PokeAttack);
+    //    pokeMon.CurrentDefense = basicStat + (int)(pokeMon.Level * pokeMon.PokeDefense);
+    //    pokeMon.CurrentHP = basicStat + (int)(pokeMon.Level * pokeMon.PokeHP);
+    //    pokeMon.CurrentSpeed = basicStat + (int)(pokeMon.Level * pokeMon.PokeSpeed);
+    //}
 }
