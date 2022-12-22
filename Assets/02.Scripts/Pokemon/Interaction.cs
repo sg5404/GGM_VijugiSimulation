b4b0f91d225b5252718fd5a275141f8de63c5c79
sg5404 <ggm.s210028@ggh.goe.go.kr>
2022-12-22 22:38:25 +0900

    에셋 추가 맵작업

--- Assets/02.Scripts/Pokemon/Interaction.cs
+++ Assets/02.Scripts/Pokemon/Interaction.cs
@@ -31,6 +31,7 @@ public class Interaction : MonoBehaviour
             foreach (Pokemon poke in agentInfo.PokemonList)
             {
                 pokemon.Heal(poke.MaxHp, poke);
+                //여기에 상대가 있었을때 상대의 말하는 스크립트를 받아와서 함수 실행
             }
         }
     }
